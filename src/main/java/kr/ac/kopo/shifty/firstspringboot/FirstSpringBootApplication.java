f2bd7ff9a049ac7c7edca1ad97a9e047f8dd4549
Shifty <jjs5827852@naver.com>
2025-03-12 10:36:22 +0900

    20250312commitTest

---
+++ src/main/java/kr/ac/kopo/shifty/firstspringboot/FirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.shifty.firstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootApplication.class, args);
+    }
+
+}
