e117509ef7fb4e3c302a843bf055a3a6b31cf447
Shifty <jjs5827852@naver.com>
2025-03-17 16:13:53 +0900

    0317

---
+++ androiddddd/app/src/main/java/kr/ac/kopo/helloooo/MainActivity.java
@@ -0,0 +1,38 @@
+package kr.ac.kopo.helloooo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        TextView text1 = findViewById(R.id.textView);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "버튼 클릭했네?", Toast.LENGTH_LONG);
+                toast.show();
+                textView.setText("shifty");
+            }
+        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+
+}
